0da7d8cb5168c8d8aa2558ba7429cd04060e2867
laioyu <laioyu@icloud.com>
2022-04-07 17:32:16 +0800

    CRLF 快捷键 + vuetify icon + slider

---
+++ src/components/CameraFov/CameraFov.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-slider
+    :ticks="tickLabels"
+    :min="30"
+    :max="120"
+    color="#55595d"
+    show-ticks="always"
+    tick-size="1.5"
+    track-fill-color="#338bff"
+    track-size="2"
+    thumb-size="10"
+    hide-details="true"
+  ></v-slider>
+  <div class="fov-labels__container">
+    <div class="fov-labels__item">
+      <v-icon style="font-size: 0.7rem">mdi-triangle</v-icon><span style="display: block">sss</span>
+    </div>
+    <div class="fov-labels__item">
+      <div><div></div></div>
+    </div>
+    <div class="fov-labels__item" style="visibility: hidden">
+      <div><div></div></div>
+    </div>
+    <div class="fov-labels__item">
+      <div><div></div></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.v-slider ::v-deep(.v-slider__container) {
+  height: 1rem;
+}
+.fov-labels__container {
+  display: flex;
+  justify-content: space-between;
+  margin-left: 8px;
+  margin-right: 8px;
+}
+.fov-labels__container .fov-labels__item {
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CameraFov",
+  data() {
+    return {
+      tickLabels: {
+        30: "",
+        60: "",
+        120: "",
+      },
+    };
+  },
+});
+</script>
---
+++ src/views/RenderView.vue
@@ -0,0 +1,18 @@
+<template>
+  <camera-fov />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+// Components
+import CameraFov from "../components/CameraFov/CameraFov.vue";
+
+export default defineComponent({
+  name: "RenderView",
+
+  components: {
+    CameraFov,
+  },
+});
+</script>
